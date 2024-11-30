6b0aadae91f5db418e43886696008b5a0d65a57a
NoroffStud-Ryu <ryuaos00900@stud.noroff.no>
2024-11-30 13:35:49 +0100

    Updated to call method and results

--- Instances.py
+++ Instances.py
@@ -1,13 +1,16 @@
 from UserInputValidator import InputValidator
 
-validator1 = InputValidator
-validator2 = InputValidator
+validator1 = InputValidator()
+validator2 = InputValidator()
 
 input1 = ["2", "b", "9000", "6.7", "40", "three"]
 input2 = ["-2", "26", "eight hunred", "546", "40", "17"]
 
 checkinput1 = validator1.validate_positive_integer(input1)
 checkinput2 = validator2.validate_positive_integer(input2)
+
+validator1.display_validation_message()
+validator2.display_validation_message()
 
 print(f"results for inputs 1: {checkinput1}")
 print(f"results for inputs 2: {checkinput2}")
